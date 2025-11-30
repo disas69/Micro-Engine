815f2dc8d2acb5865ca389c785f4d032718f05db
Eugene <38591750+disas69@users.noreply.github.com>
2025-11-30 17:31:19 +0100

    Add Engine and Editor examples

--- Editor/Source/main.cpp
+++ Editor/Source/main.cpp
@@ -1,8 +1,47 @@
 #include "EditorApp.h"
+#include "raylib-cpp.hpp"
+#include "imgui.h"
+#include "rlImGui.h"
 
 int main()
 {
     Micro::EditorApp editor;
+
+	int screenWidth = 1280;
+	int screenHeight = 800;
+
+    raylib::Window window(screenWidth, screenHeight, "raylib-Extras [ImGui] example - simple ImGui Demo");
+	window.SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+	window.SetTargetFPS(144);
+
+    rlImGuiSetup(true);
+
+	// Main game loop
+	while (!window.ShouldClose())    // Detect window close button or ESC key
+	{
+		window.BeginDrawing();
+		window.ClearBackground(DARKGRAY);
+
+		// start ImGui Conent
+		rlImGuiBegin();
+
+		bool open = true;
+		ImGui::ShowDemoWindow(&open);
+
+		open = true;
+		if (ImGui::Begin("Test Window", &open))
+		{
+			ImGui::TextUnformatted(ICON_FA_JEDI);
+		}
+		ImGui::End();
+
+		// end ImGui Content
+		rlImGuiEnd();
+
+		window.EndDrawing();
+	}
+
+    rlImGuiShutdown();
 
     return 0;
 }
--- Engine/Source/main.cpp
+++ Engine/Source/main.cpp
@@ -1,8 +1,25 @@
 #include "Core/Engine.h"
+#include "raylib-cpp.hpp"
 
 int main()
 {
     Micro::Engine engine;
+
+    int screenWidth = 800;
+    int screenHeight = 450;
+
+    raylib::Window window(screenWidth, screenHeight, "raylib-cpp - basic window");
+    window.SetTargetFPS(60);
+
+    while (!window.ShouldClose())
+    {
+        window.BeginDrawing();
+        window.ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        window.EndDrawing();
+    }
 
     return 0;
 }
