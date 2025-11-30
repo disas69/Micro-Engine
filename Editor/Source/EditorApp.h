f12ce418461a0b68d2512d956ef84280abb229a5
Eugene <38591750+disas69@users.noreply.github.com>
2025-11-30 18:04:10 +0100

    Refactor editor example

--- Editor/Source/EditorApp.h
+++ Editor/Source/EditorApp.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include "raylib-cpp.hpp"
+#include "imgui.h"
+
 namespace Micro
 {
 class EditorApp
 {
 public:
     EditorApp();
+    int Run();
+
+private:
+    raylib::Window m_window;
+    ImVec2 m_mainViewWidth = ImVec2(650.0f, 0.0f);
+    ImVec2 m_toolbarWidth = ImVec2(260.0f, 0.0f);
+    ImVec2 m_contentHeight = ImVec2(0.0f, 360.0f);
 };
 }  // namespace Micro
